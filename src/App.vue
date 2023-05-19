3664edd39e4aa59e3bfbc720f0ee3da35c9ac904
Aibek002 <200103346@stu.sdu.edu.kz>
2023-05-19 18:33:06 +0600

    changes

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,25 @@
 <template>
 <div class="body">
-    
-    <Header />
+    <router-view/>
+    <!-- <Header />
     <Title />
     <About />
     <Hotels />
-    <Tours />
+    <Tours /> -->
 </div>
 
 </template>
 
 
 <script>
-import Header from './components/Header.vue';
-import Title from './components/Title.vue'
-import About from './components/About.vue';
-import Hotels from './components/Hotels.vue';
-import Tours from './components/Tours.vue';
+// import Header from './components/Header.vue';
+// import Title from './components/Title.vue'
+// import About from './components/About.vue';
+// import Hotels from './components/Hotels.vue';
+// import Tours from './components/Tours.vue';
 export default{
     components:{
-        Header,Title,About,Hotels,Tours
+        // Header,Title,About,Hotels,Tours
     },
 
     data(){
